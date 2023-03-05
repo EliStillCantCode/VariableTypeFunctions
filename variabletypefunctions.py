a462038571a48d0fa9e6a00f029d9a7999a3343b
EliStillCantCode <n4thenknowsdawae442@gmail.com>
2023-03-05 13:17:02 +0000

    add license mention

--- variabletypefunctions.py
+++ variabletypefunctions.py
@@ -1,1 +1,3 @@
-# This code is licensed
+# This code is licensed under the MIT License, view the file "LICENSE" or go to
+# https://github.com/EliStillCantCode/VariableTypeFunctions/blob/master/LICENSE
+# To see restrictions and permissions
